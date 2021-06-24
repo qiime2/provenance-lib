0368536943ad22413e7291c2055c158f58f8d8be
Chris Keefe <crk239@nau.edu>
2021-06-24 12:25:22 -0700

    "Fixes" relative imports in runner.py

--- provenance_lib/runner.py
+++ provenance_lib/runner.py
@@ -1,6 +1,13 @@
+# flake8: noqa
 import sys
+import zipfile
 
-import parse
+sys.path.append('/home/chris/src/provenance_py')
+
+if __name__ == "__main__" and __package__ is None:
+    __package__ = "provenance_lib"
+
+from .parse import ProvDAG
 
 if __name__ == '__main__':
     # To begin, we'll read in exactly one fp
@@ -8,7 +15,7 @@ if __name__ == '__main__':
         raise ValueError('Please pass one filepath to a QIIME 2 Archive')
 
     archive_fp = sys.argv[1]
-    dummy_DAG = parse.ProvDAG(archive_fp)
+    dummy_DAG = ProvDAG(archive_fp)
 
     r_uuid = dummy_DAG.root_uuid
     deets = dummy_DAG.get_result(r_uuid)._action._action_details
@@ -18,7 +25,7 @@ if __name__ == '__main__':
     print(f'{repr(dummy_DAG)}')
     print(f'{dummy_DAG._archive_md}')
     print(f'- was made by q2-{plurg} {ackshun}')
-    print(f'- contains prov. data from {dummy_DAG._number_of_results}'
+    print(f'- contains prov. data from {dummy_DAG._num_results}'
           ' QIIME 2 Results, mostly ancestors')
     # print(dummy_DAG._archive_contents)
 

1b0fe98227b11db324c49492200f7241e4405bec
Chris Keefe <crk239@nau.edu>
2021-12-14 18:54:05 -0700

    MAINT: Adds MYPY as a dep

--- setup.py
+++ setup.py
@@ -18,5 +18,7 @@ setup(
    license='BSD-3-clause',
    packages=find_packages(),
    install_requires=['pyyaml>=5.3', 'bibtexparser>=1.0', 'pytest>=6',
-                     'pytest-cov>=2.0', 'flake8', 'pandas', 'networkx'],
+                     'pytest-cov>=2.0', 'flake8', 'pandas', 'networkx',
+                     'mypy',
+                     ],
 )

5e8e447e89f798cfbbb2dd87a1c20257011a8d54
Chris Keefe <crk239@nau.edu>
2022-03-01 16:01:27 -0700

    Don't cover/lint temporary runner scripts

---
+++ playtesting/playtesting_write_fp.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# flake8: noqa
+from provenance_lib.replay import replay_fp
+replay_fp(
+    "/home/chris/src/provenance_py/provenance_lib/tests/data/parse_dir_test",
+    "./playtesting/replay_uu.py",
+    usage_driver_name="python3",
+)

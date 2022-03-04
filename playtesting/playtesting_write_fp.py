58e5ac62268bfb03e25f73b5258b13014b564fdf
Chris Keefe <crk239@nau.edu>
2022-03-04 16:04:36 -0700

    "Removes" playtesting files

--- playtesting/playtesting_write_fp.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-# flake8: noqa
-from provenance_lib.replay import replay_fp
-replay_fp(
-    "/home/chris/src/provenance_py/provenance_lib/tests/data/parse_dir_test",
-    "./playtesting/replay_uu.py",
-    usage_driver_name="python3",
-)

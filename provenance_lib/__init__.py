711fc5da155e53c8723b9165d8c8a660476121c1
Chris Keefe <crk239@nau.edu>
2022-04-17 22:27:31 -0700

    exposes write_reproducibility_supp in prov_lib __init__

--- provenance_lib/__init__.py
+++ provenance_lib/__init__.py
@@ -1,11 +1,13 @@
 from .parse import ProvDAG, archive_not_parsed, UnparseableDataError
-from .replay import replay_provenance, write_citations
+from .replay import (
+    replay_provenance, write_citations, write_reproducibility_supplement,
+)
 from .util import get_root_uuid, get_nonroot_uuid, camel_to_snake
 from .version_parser import parse_version_from_fp, parse_version
 
 __all__ = [
     'ProvDAG', 'archive_not_parsed', 'UnparseableDataError',
     'get_root_uuid', 'get_nonroot_uuid', 'camel_to_snake',
-    'replay_provenance', 'write_citations', 'parse_version_from_fp',
-    'parse_version',
+    'replay_provenance', 'write_citations', 'write_reproducibility_supplement',
+    'parse_version_from_fp', 'parse_version',
 ]
